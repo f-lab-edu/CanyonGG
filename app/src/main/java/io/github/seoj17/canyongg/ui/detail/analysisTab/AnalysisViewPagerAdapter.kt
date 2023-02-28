6cc1bd15fcfefe4a6b063395fdbc5cc73275ac70
seojeongwoo <qw4643987@naver.com>
2023-03-01 02:45:17 +0900

    #86 :hammer: 뷰 페이저 어댑터 Enum 클래스로 코드 정리함

--- app/src/main/java/io/github/seoj17/canyongg/ui/detail/analysisTab/AnalysisViewPagerAdapter.kt
+++ app/src/main/java/io/github/seoj17/canyongg/ui/detail/analysisTab/AnalysisViewPagerAdapter.kt
@@ -2,30 +2,16 @@ package io.github.seoj17.canyongg.ui.detail.analysisTab
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import io.github.seoj17.canyongg.ui.detail.analysisTab.pages.damaged.TeamDamagedFragment
-import io.github.seoj17.canyongg.ui.detail.analysisTab.pages.dealt.TeamDealtFragment
-import io.github.seoj17.canyongg.ui.detail.analysisTab.pages.gold.TeamGoldFragment
-import io.github.seoj17.canyongg.ui.detail.analysisTab.pages.kill.TeamKillsFragment
-import io.github.seoj17.canyongg.ui.detail.analysisTab.pages.minion.TeamMinionFragment
-import io.github.seoj17.canyongg.ui.detail.analysisTab.pages.vision.TeamVisionScoreFragment
 
 
 class AnalysisViewPagerAdapter(
     fragment: Fragment,
-    matchId: String,
 ) : FragmentStateAdapter(fragment) {
 
-    private val tabs: List<Fragment> = listOf(
-        TeamKillsFragment.newInstance(matchId),
-        TeamDealtFragment.newInstance(matchId),
-        TeamDamagedFragment.newInstance(matchId),
-        TeamGoldFragment.newInstance(matchId),
-        TeamMinionFragment.newInstance(matchId),
-        TeamVisionScoreFragment.newInstance(matchId),
-    )
+    private val tabs = TeamAnalysisTabs.values()
 
     override fun getItemCount(): Int = tabs.size
 
-    override fun createFragment(position: Int): Fragment = tabs[position]
+    override fun createFragment(position: Int): Fragment = tabs[position].newInstance
 
 }

d9ec23afa07157c7dae3ec4035594d76af05d432
SEO-J17 <qw4643987@naver.com>
2023-03-04 00:12:51 +0900

    #86 :hammer: 바뀐이름으로 코드 적용함

--- app/src/main/java/io/github/seoj17/canyongg/data/repository/SummonerRepository.kt
+++ app/src/main/java/io/github/seoj17/canyongg/data/repository/SummonerRepository.kt
@@ -1,6 +1,6 @@
 package io.github.seoj17.canyongg.data.repository
 
-import io.github.seoj17.canyongg.data.local.recent.search.RecentSearchNameEntity
+import io.github.seoj17.canyongg.data.model.RecentSearchNameDataModel
 import io.github.seoj17.canyongg.data.model.SummonerDataModel
 import io.github.seoj17.canyongg.data.model.SummonerTierDataModel
 import kotlinx.coroutines.flow.Flow
@@ -8,7 +8,7 @@ import kotlinx.coroutines.flow.Flow
 interface SummonerRepository {
     suspend fun getSummonerInfo(userName: String): SummonerDataModel?
     suspend fun getTier(id: String): SummonerTierDataModel?
-    fun getRecentSummoner(): Flow<List<RecentSearchNameEntity>>
+    fun getRecentSummoner(): Flow<List<RecentSearchNameDataModel>>
     suspend fun getRotationChamps(): List<Int>
     suspend fun addRecentSummoner(puuid: String, name: String)
     suspend fun deleteRecentSummoner(name: String)
